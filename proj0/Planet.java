a33b5f753bd659fd546902604b93c87b8a5cbba8
rockherojw <rockherojw@gmail.com>
2017-10-30 17:50:34 -0700

    Finish Planet.java

--- proj0/Planet.java
+++ proj0/Planet.java
@@ -29,4 +29,53 @@ public class Planet {
         return distance;
     }
 
+    public double calcForceExertedBy(Planet p) {
+        double G = 6.67 * Math.pow(10, -11);
+        double distance = this.calcDistance(p);
+        double force = G * this.mass * p.mass / Math.pow(distance, 2);
+        return force;
+    }
+
+    public double calcForceExertedByX(Planet p) {
+        double Xforce = this.calcForceExertedBy(p) * (p.xxPos - this.xxPos) / this.calcDistance(p);
+        return Xforce;
+    }
+
+    public double calcForceExertedByY(Planet p) {
+        double Yforce = this.calcForceExertedBy(p) * (p.yyPos - this.yyPos) / this.calcDistance(p);
+        return Yforce;
+    }
+
+    public double calcNetForceExertedByX(Planet[] p_array){
+        double NetForceX = 0;
+        for (int i=0; i < p_array.length; i++) {
+            Planet p = p_array[i];
+            if (!this.equals(p)) {
+                NetForceX += this.calcForceExertedByX(p);
+            }
+        }
+         return NetForceX;
+    }
+
+    public double calcNetForceExertedByY(Planet[] p_array){
+        double NetForceY = 0;
+        for (int i=0; i < p_array.length; i++) {
+            Planet p = p_array[i];
+            if (!this.equals(p)) {
+                NetForceY += this.calcForceExertedByY(p);
+            }
+        }
+         return NetForceY;
+    }
+
+    public void update(double dt, double fx, double fy) {
+        double accX = fx / this.mass;
+        double accY = fy / this.mass;
+        this.xxVel += dt * accX;
+        this.yyVel += dt * accY;
+        this.xxPos += dt * this.xxVel;
+        this.yyPos += dt * this.yyVel;
+    }
+
 }
+
