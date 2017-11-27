556878fb98e203000cb525259076116d8817149b
rockherojw <rockherojw@gmail.com>
2017-11-27 13:19:38 -0800

    Paused ArrayDeque

--- proj1a/ArrayDeque.java
+++ proj1a/ArrayDeque.java
@@ -54,13 +54,60 @@ public class ArrayDeque<T> {
     }
 
     public void printDeque(){
-
         while(nextFirst != nextLast+1) {
-            System.out.println(item[nextFirst]);
+            if(nextFirst == item.length - 1) {
+                System.out.println(item[nextFirst]);
+                nextFirst = 0;
+            } else {
+                System.out.println(item[nextFirst]);
+            }
             nextFirst += 1;
         }
     }
+// STOP HERE at 11/28/17
 
+    public T removeFirst(){
+        T firstReturn = item[nextInQue(nextFirst,size)];
+        item[nextInQue(nextFirst,size)] = null;
+        nextFirst = nextInQue(nextFirst,size);
+        size -= 1;
+        halve();
+        return firstReturn;
+    }
+
+    public T removeLast(){
+        T lastReturn = item[preInQue(nextLast,size)];
+        item[preInQue(nextLast,size)] = null;
+        nextFirst = preInQue(nextLast,size);
+        size -= 1;
+        halve();
+        return lastReturn;
+    }
+
+    private int nextInQue(int i, int size){
+        if (i ==size){
+            return 0;
+        }
+        else {return i+1;}
+    }
+
+    private int preInQue(int i, int size){
+        if (i == 0){
+            return size;
+        }
+        else {return i-1;}
+    }
+
+    private void halve(){
+        double R = 0.25;
+        double usageRatio = size / item.length;
+        if (usageRatio < R && size > 16){
+            int factor = 2;
+            T[] newItem = (T[]) new Object[size % factor];
+            System.arraycopy(item, 0, newItem, 0, size);
+            item = newItem;
+        }
+    }
 
 }
 
